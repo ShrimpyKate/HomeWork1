edb0822234086a67df5330fab645209f283a8fb8
Kate <katekrupnik@gmail.com>
2023-01-06 16:09:23 +0300

    улучшила программу ex01

--- ex01/Program.cs
+++ ex01/Program.cs
@@ -1,13 +1,18 @@
 ﻿//  Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-int a = 88;
-int b = 78;
+int a = 200;
+int b = 111;
 int max = b;
 int min = a;
-if (a > b) max = a;
-if (a < b) min = a;
-if (b < a) min = b;
-if (b > a) max = b;
+if (a > b) 
+  {
+  max = a; min = b;
+  }
+else
+  {
+  min = a; max = b;
+  }
+
 
 Console.Write( "max =");
 Console.WriteLine( max );
