bfbf75bb6f52049db964949d8f80804b0f05aced
Kate <katekrupnik@gmail.com>
2023-01-06 15:33:15 +0300

    написали программу для ex01

--- ex01/Program.cs
+++ ex01/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+int a = 88;
+int b = 78;
+int max = b;
+int min = a;
+if (a > b) max = a;
+if (a < b) min = a;
+if (b < a) min = b;
+if (b > a) max = b;
+
+Console.Write( "max =");
+Console.WriteLine( max );
+Console.Write( "min =");
+Console.WriteLine (min);
+
