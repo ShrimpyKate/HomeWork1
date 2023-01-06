60eb3b48e1cf25c3b4a1e65617133b5a961345b2
Kate <katekrupnik@gmail.com>
2023-01-06 17:03:43 +0300

    написали программу для ex02

--- ex02/Program.cs
+++ ex02/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int a = 13;
+int b = 11;
+int c = 77;
+
+int max = a;
+
+if ( b > max ) max = b;
+if (c > max ) max = c;
+
+Console.Write($"max = {max}");
+
+
